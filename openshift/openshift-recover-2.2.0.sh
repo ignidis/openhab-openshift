53bcc58a7310b5f178344abfc78bbd1961a40c84
ignidis <patch@cybearth.net>
2019-10-07 10:25:22 +0200

    remove obsolete files

--- openshift/openshift-recover-2.2.0.sh
+++
@@ -1,6 +0,0 @@
- oc new-build "2.2.0/amd64/centos" --name="openhab" --to="openhab:2.2.0-amd64-centos" && \
- oc start-build openhab --from-dir "2.2.0/amd64/centos" && \
- oc adm policy add-scc-to-user anyuid system:serviceaccount:openhab:default && \
- oc create -f openhab-deployment.yaml && \
- oc create -f openhab-service.yaml && \
- oc create -f openhab-routes.yaml
